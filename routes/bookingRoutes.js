256e22fa260e165971d85fb3d520a637074d9573
LosBandidox <153774406+LosBandidox@users.noreply.github.com>
2024-11-26 22:39:17 +0300

    Updated bookingRoutes.js

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,10 +1,13 @@
+
 const express = require('express');
 const router = express.Router();
 const bookingController = require('../controllers/bookingController');
 
-// Create a new booking
-router.post('/bookings', bookingController.createBooking);
+// Route to fetch all bookings
+router.get('/bookings', bookingController.getAllBookings);
 
+// Route to create a new booking
+router.post('/bookings', bookingController.createBooking);
 // Get booking details
 router.get('/bookings/:id', bookingController.getBooking);
 
