7b55ef7a2980ab312f8ae7f2b6c781546a11c452
LosBandidox <153774406+LosBandidox@users.noreply.github.com>
2024-11-26 22:36:41 +0300

    Updated userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 
-// routes/userRoutes.js
-
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-// Create a new user
+// Route to fetch all users
+router.get('/users', userController.getAllUsers);
+
+// Route to create a new user
 router.post('/users', userController.createUser);
 
 module.exports = router;
